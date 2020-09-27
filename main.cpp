f5dde1d8650b83203f2a9254f32744845d456bfa
Dario <dariosantelli@gmail.com>
2020-09-27 15:08:19 -0400

    Cleaned up ReadFromFile function, improved Recipe print(), added GUI layout and beginnings of GUI code

--- main.cpp
+++ main.cpp
@@ -12,13 +12,14 @@ public:
     string recipe_name {};
     string ingredients {};
     string descriptors {};
-    int ingred[3] {4, 5, 6};
 
     void print()
     {
-        cout << "Ingredients:" << endl;
-        for (int i = 0; i<5; i++)
-            cout << i << endl;
+        cout << endl;
+        cout << "Recipe: " << recipe_name << endl;
+        cout << "Ingredients: " << ingredients << endl;
+        cout << "Decriptions: " << descriptors << endl;
+        cout << endl;
     }
 };
 
@@ -29,13 +30,10 @@ void OpenFile()
     myfile.open ("example.txt", ios::out | ios::app);
     getline(myfile, line);
     cout << line << endl;
-    //myfile << "I wrote this f\n";
-    //ofstream get
 }
 
 //need to implement read and append functions to text file, then search and modify functions for those entries
 //reads all data from text file into separate objects and stores in memory
-//can create new recipe and add to text file
 //format of data in text file: RecipeName, Ingredients, Descriptor /n
 
 //Delimit target with ',' and modify output vector
@@ -49,23 +47,34 @@ void delimit(string const &str, vector<std::string> &out)
 	}
 }
 
-//Read each line from the text file, create a recipe object from each line
-void ReadFromFile()
+//Reads each line of file and creates a recipe object for each, adds these objects to results vector
+void ReadFromFile(vector<Recipe> &results)
 {
+    Recipe obj [3] {};
     string line;
     ifstream myfile ("example.txt");
     if (myfile.is_open())
     {
         while ( getline (myfile,line) )
         {
+            Recipe working {};
             std::vector<std::string> out;
             delimit(line, out);
-            cout << line << '\n';
-            for (int i = 0; i<3; i++)
-                cout << out[i] << endl;
+            working.recipe_name = out[0];
+            working.ingredients = out[1];
+            working.descriptors = out[2];
+
+            results.push_back(working);
+            //cout << obj << endl;
+            /*
+            cout << "Working Recipe Name: " << working.recipe_name << endl;
+            cout << "Working Ingredients: " << working.ingredients << endl;
+            cout << "Working Descriptors: " << working.descriptors << endl;
+            */
         }
-        myfile.close();
+
     }
+
 }
 
 //Add a new entry to the text file in the form "RecipeName,Ingredients,Descriptor"
@@ -110,15 +119,43 @@ int main()
             cout << out[i] << endl;
     */
     //OpenFile();
-    ReadFromFile();
+    vector<Recipe> results {};
+    ReadFromFile(results);
+    for(int i=0; i < results.size(); i++)
+        cout << results.at(i).ingredients << ' ';
     //NewEntry();
+    results.at(0).print();
     //Recipe test {};
     //test.ingredients = "fries";
     //cout << test.ingredients << endl;
     //test.print();
-    //cout << test.ingred[0];
-    //string test [2] {delimit("dog")};
-    //test = delimit("dog");
-    //cout << delimit("dog");
+
+    /*
+    int input {};
+    cin >> input;
+    if (input == 1)   {
+        cout << "Input is: " << input << "(should be 1)" << endl;
+    }   else if (input == 2)  {
+            cout << "Input is: " << input << "(should be 2)" << endl;
+    }   else if (input == 3)  {
+            cout << "Input is: " << input << "(should be 3)" << endl;
+    }   else if (input == 8)  {
+            cout << "Input is: " << input << "(should be 8)" << endl;
+    }   else if (input == 9)  {
+            cout << "Input is: " << input << "(should be 9)" << endl;
+    }   else {
+        cout << "Invalid number entered, try again." << endl;
+    }
     return 0;
+    */
 }
+
+/*
+GUI Layout:
+Press a number on the keypad and press enter for more options:
+1: View recipes
+2: Add a recipe
+3: Edit an existing recipe
+8: Tutorial/Help
+9: Exit program
+*/
